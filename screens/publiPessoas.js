65bce5e7d1c04518d3c57906a7d7c528eb3b0ee6
Felipe do Prado <felippe_praddo287@hotmail.com>
2022-10-07 14:32:09 -0300

    Ajustado layout da Tela PubliPessoas

--- screens/publiPessoas.js
+++ screens/publiPessoas.js
@@ -1,5 +1,6 @@
-import * as React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {View, Image, StyleSheet,TouchableOpacity,  ScrollView} from 'react-native';
+const listaPerfil = require('./dados/publiPessoas.json');
 
 function PubliPessoaScreen({navigation}) {
   function buttonPress() {
@@ -7,22 +8,43 @@ function PubliPessoaScreen({navigation}) {
   }
 
   const styles = StyleSheet.create({
-    container: {
+    scroll: {
       flex: 1,
       backgroundColor: '#000000',
-      alignItems: 'center',
-      justifyContent: 'center',
     },
-    text: {
-      color: '#ffffff',
-      fontSize: 16,
-      marginTop: 10,
+    stilee2: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      marginLeft: 5,
+      marginTop: 5,
+    },
+    imagPost: {
+      marginTop: 5,
+      width: 140,
+      height: 120,
+      borderColor: '#000000',
+      borderWidth: 3,
     },
   });
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}> Publicação com pessoas</Text>
-    </View>
+    <View style={styles.scroll}>
+    <ScrollView style={styles.scroll}>
+      <View>
+        <View style={styles.stilee2}>
+          {listaPerfil.postagensPessoas.length > 0 &&
+            listaPerfil.postagensPessoas.map(item => {
+              return (
+                <TouchableOpacity onPress={() => {}}>
+                <Image style={styles.imagPost}
+                  source={{ uri: item.imagem }}
+                  resizeMode="stretch"/>
+                </TouchableOpacity>
+              );
+            })}
+        </View>
+      </View>
+    </ScrollView>
+  </View>
   );
 }
 export default PubliPessoaScreen;
