f3778e0b7d5600032cab33cf47d4bdfc37f6b80c
Felipe do Prado <felippe_praddo287@hotmail.com>
2022-10-29 16:59:06 -0300

    Implementado Firestore em publiVideos
    Implementado banco de dados Firestore

--- screens/publiVideos.js
+++ screens/publiVideos.js
@@ -1,86 +1,67 @@
-import React, { useState }from 'react';
-import {StyleSheet, Text, ScrollView, TouchableWithoutFeedback, View} from 'react-native';
+import React, { useState, useEffect }from 'react';
+import {Text, TouchableWithoutFeedback, ActivityIndicator, 
+  FlatList, View} from 'react-native';
 import Video from 'react-native-video';
 import Feather from 'react-native-vector-icons/Feather';
+import firestore from '@react-native-firebase/firestore';
+import StyPubliVideos from './styless/styPubliVideos';
 
-const listaPubliVideo = require('./dados/publiVideos .json')
 
-function PubliVideoScreen({navigation}) {
-  function buttonPress() {
-    navigation.goBack();
-  }
+function PubliVideoScreen() {
   const [paused1, setPaused1] = useState(false)
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
 
    const onPlayPausePress = ()=> {
    setPaused1(!paused1)
    }
-  const styles = StyleSheet.create({
-    container: {
-    width: '100%',
-    height: '100%',
-    },
-    container1: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      },
-    container2:{
-     marginTop:260,
-     width: 140,
-     height: 40,
-     flexDirection: 'row',
-    },
-    containerVideo: {
-      marginTop:5,
-      marginLeft: 5,
-        width: '32%',
-        height: 300,
-    },
-    backgroundVideo: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
-    },
-    text: {
-      color: '#ffffff',
-      fontSize: 16,
-      alignSelf: 'center',
-      marginLeft: 10,
-    },
-    icon1: {
-      color: '#ffffff',
-      fontSize: 13,
-      alignSelf: 'center',
-      marginLeft: 10,
-      
-    },
-  });
+
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('videoPostagem')
+      .onSnapshot(querySnapshot => {
+        const users = [];
+  
+        querySnapshot.forEach(documentSnapshot => {
+          users.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+  
+        setUsers(users);
+        setLoading(false);
+      });
+
+    return () => subscriber();
+  }, []);
+
+  if (loading) {
+    return (<View style={StyPubliVideos.container} ><ActivityIndicator /></View>) ;
+  } 
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <View style={styles.container1}>
-        {listaPubliVideo.postagensVideos.length > 0 && listaPubliVideo.postagensVideos.map(item=>{
-          return(
+    <FlatList
+    data={users}
+    numColumns='3'
+    renderItem={({ item }) => (
+        <View style={StyPubliVideos.container1}>
             <TouchableWithoutFeedback onPress={onPlayPausePress}>
-            <View style={styles.containerVideo}>
+            <View style={StyPubliVideos.containerVideo}>
               <Video source={{uri:item.videoPostagem}} 
-            style={styles.backgroundVideo}
+            style={StyPubliVideos.backgroundVideo}
            resizeMode={'stretch'}
            repeat={true}
            paused={paused1}
           />
-           <View style={styles.container2}>
-           <Feather name="play" style={styles.icon1}/>
-           <Text style={styles.text}>{item.numer1}</Text>
+           <View style={StyPubliVideos.container2}>
+           <Feather name="play" style={StyPubliVideos.icon1}/>
+           <Text style={StyPubliVideos.text}>{item.numer1}</Text>
           </View>
          </View>
          </TouchableWithoutFeedback>
-          )
-        })}
         </View>
-      </ScrollView>
-    </View>
+              )}
+              />
   );
 }
 export default PubliVideoScreen;
